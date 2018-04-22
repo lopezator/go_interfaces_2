31bdcb898a37ca824d6008150c7c6fd68adc3969
David López <dlopbec@gmail.com>
2018-04-22 11:48:18 +0200

    Make an HTTP Request and print out to terminal the Response.

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"fmt"
+	"os"
+)
 
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	fmt.Println(resp)
 }
